1279d4e3c0f0238044c16efc5759051f9b1e79f3
pancakecat <1105855277@qq.com>
2018-05-14 15:23:34 +0800

    Add files via upload
    change css of login and parkingIndex

---
+++ components/admin/AdminManger.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <el-table
+    ref="multipleTable"
+    :data="tableData3"
+    tooltip-effect="dark"
+    style="width: 100%"
+    @selection-change="handleSelectionChange">
+    <el-table-column
+      type="selection"
+      width="55">
+    </el-table-column>
+    <el-table-column
+      label="日期"
+      width="120">
+      <template slot-scope="scope">{{ scope.row.date }}</template>
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="120">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="地址"
+      show-overflow-tooltip>
+    </el-table-column>
+  </el-table>
+    <div class="admin-user-operate">
+      <el-button type="primary">增加</el-button>
+      <el-button type="primary" @click="dialogVisible=true">删除</el-button>
+    </div>
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
+      <span>是否确定删除选择的用户？</span>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+  </span>
+</el-dialog>
+    <el-pagination background layout="prev, pager, next" :total="1">
+    </el-pagination>
+  </div>
+    
+</template>
+<script>
+export default {
+  data() {
+    return {
+      dialogVisible:false,
+      tableData3: [],
+        multipleSelection: []
+    }
+  },
+
+  methods: {
+    toggleSelection(rows) {
+        if (rows) {
+          rows.forEach(row => {
+            this.$refs.multipleTable.toggleRowSelection(row);
+          });
+        } else {
+          this.$refs.multipleTable.clearSelection();
+        }
+      },
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
+      }
+  }
+}
+
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.admin-user-operate {
+  margin-top: 30px;
+  text-align: right;
+}
+
+.el-table {
+  margin-top: 20px;
+}
+
+.el-pagination {
+  width: auto;
+  text-align: center;
+  margin: 50px auto;
+}
+
+</style>
---
+++ components/admin/AdminUser.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="admin-user">
+    <el-dropdown>
+      <el-button type="primary">
+        {{userType}}<i class="el-icon-arrow-down el-icon--right"></i>
+      </el-button>
+      <el-dropdown-menu slot="dropdown">
+        <router-link to="/AdminManger">
+          <el-dropdown-item>运营商</el-dropdown-item>
+        </router-link>
+        <router-link to="/AdminOperator">
+          <el-dropdown-item>操作员</el-dropdown-item>
+        </router-link>
+      </el-dropdown-menu>
+    </el-dropdown>
+    <div class="admin-user-search">
+      <el-input v-model="input" placeholder="用户名"></el-input>
+      <el-button type="primary">查找</el-button>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      tableData3: [],
+      multipleSelection: [],
+      userType: "用户角色"
+    }
+  },
+
+  methods: {
+    toggleSelection(rows) {
+      if (rows) {
+        rows.forEach(row => {
+          this.$refs.multipleTable.toggleRowSelection(row);
+        });
+      } else {
+        this.$refs.multipleTable.clearSelection();
+      }
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val;
+    }
+  }
+}
+
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+  text-decoration: none;
+}
+
+.admin-user {
+  width: 980px;
+  height: 90%;
+}
+
+.admin-user-search {
+  float: right;
+}
+
+.admin-user-operate {
+  margin-top: 30px;
+  text-align: right;
+}
+
+.el-table {
+  margin-top: 20px;
+}
+
+.el-input {
+  display: inline-block;
+  width: 200px;
+}
+
+.el-dropdown {
+  vertical-align: top;
+}
+
+.el-dropdown+.el-dropdown {
+  margin-left: 15px;
+}
+
+.el-icon-arrow-down {
+  font-size: 12px;
+}
+
+.el-pagination {
+  width: auto;
+  text-align: center;
+  margin: 50px auto;
+}
+
+</style>
