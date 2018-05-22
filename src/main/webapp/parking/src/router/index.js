91003e51b827ed8200362a95c2644c00b85afc1a
pancakecat <1105855277@qq.com>
2018-05-22 09:42:16 +0800

    Add files via upload
    update router

--- src/main/webapp/parking/src/router/index.js
+++ src/main/webapp/parking/src/router/index.js
@@ -6,6 +6,14 @@ import AdminPaking from '@/components/admin/AdminPaking'
 import AdminUser from '@/components/admin/AdminUser'
 import AdminManger from '@/components/admin/AdminManger'
 import AdminOperator from '@/components/admin/AdminOperator'
+// Actor经理
+import ManagerIndex from '@/components/manager/ManagerIndex'
+import PriceManage from '@/components/manager/PriceManage'
+import SellManage from '@/components/manager/SellManage'
+//Actor操作员
+import OperatorIndex from '@/components/operator/OperatorIndex'
+import CreateCard from '@/components/operator/CreateCard'
+import SearchCard from '@/components/operator/SearchCard'
 
 Vue.use(Router)
 
@@ -42,6 +50,46 @@ export default new Router({
             }
           ]
         }
+      ]
+    },
+    {
+      // 经理管理页面
+      path: '/ManagerIndex',
+      redirect: '/ManagerIndex/PriceManage',
+      name: 'ManagerIndex',
+      component: ManagerIndex,
+      children: [{
+        //价格管理
+          path: '/ManagerIndex/PriceManage',
+          name: 'PriceManage',
+          component: PriceManage
+        },
+        //销售管理
+        {
+          path: '/ManagerIndex/SellManage/:lotName',
+          name: 'SellManage',
+          component: SellManage
+        },
+      ]
+    },
+    {
+      // 操作员管理页面
+      path: '/OperatorIndex',
+      redirect: '/OperatorIndex/CreateCard',
+      name: 'OperatorIndex',
+      component: OperatorIndex,
+      children: [{
+        //创建新卡
+          path: '/OperatorIndex/CreateCard',
+          name: 'CreateCard',
+          component: CreateCard
+        },
+        //停车卡查询
+        {
+          path: '/OperatorIndex/SearchCard',
+          name: 'SearchCard',
+          component: SearchCard
+        },
       ]
     },
   ]
