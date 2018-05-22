5d08e8ff445f684b544bbe8a1a9de0465b269135
pancakecat <1105855277@qq.com>
2018-05-22 09:44:06 +0800

    Add files via upload
    add manager and operator

---
+++ src/main/webapp/parking/src/components/operator/SearchCard.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="search-card">
+		<el-input v-model="searchNumber" placeholder="请输入手机号"></el-input>
+		<el-button type="primary">查找</el-button>
+		<el-table
+          :data="benefitData"
+          style="width: 80%">
+          <el-table-column
+            prop="rend"
+            label="租金"
+            width="200">
+          </el-table-column>
+          <el-table-column
+            prop="totalBenefit"
+            label="收入总额"
+            width="200">
+          </el-table-column>
+          <el-table-column
+            prop="expectBenefit"
+            label="期望盈利">
+          </el-table-column>
+        </el-table>
+	</div>
+</template>
+
+<script>
+	export default{
+		name: `SearchCard`,
+		data(){
+			return{
+				searchNumber: ``,
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.search-card{
+
+	}
+	.el-input {
+		display: inline-block;
+		width: 270px;
+		margin-bottom: 30px;
+	}
+</style>
